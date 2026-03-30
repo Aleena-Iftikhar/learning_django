bf2780dbd3dfabe6d21350dcbb6673eed27d8228
Aleena Iftikhar <aleenaiftikhar1217@gmail.com>
2026-03-30 19:39:35 +0500

    ORM

--- vege/admin.py
+++ vege/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(recipe)
+
+admin.site.register(Department)
+admin.site.register(studentID)
+admin.site.register(Student)
--- vege/models.py
+++ vege/models.py
@@ -9,3 +9,35 @@ class recipe(models.Model):
     recipe_name = models.CharField(max_length=50)
     description = models.TextField()
     image = models.ImageField(upload_to='Recipes')
+
+
+class Department(models.Model):
+    department = models.CharField(max_length=50)
+
+    def __str__(self)-> str:
+        return self.department
+    
+    class Meta:
+        ordering = ['department']
+
+
+class studentID(models.Model):
+    student_id = models.CharField(max_length=50)
+
+    def __str__(self)-> str:
+        return self.student_id
+    
+class Student(models.Model):
+    department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
+    student_id = models.OneToOneField(studentID, related_name="studentid", on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=100)
+    student_email = models.EmailField(unique=True)
+    student_age = models.IntegerField(default=18)
+    student_address = models.TextField()
+
+    def __str__(self) -> str:
+        return self.student_name
+
+    class Meta:
+        ordering = ['student_name']
+        verbose_name = "student"
